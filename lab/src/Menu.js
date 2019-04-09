ee31419dd7a909ec3849912041edc2135d367e19
Daniel Cavalcante <danielcdesouza@gmail.com>
2019-04-09 12:18:25 -0300

    Add component Input

--- lab/src/Menu.js
+++ lab/src/Menu.js
@@ -9,8 +9,12 @@ import Platform from './components/Platform'
 import PropsValidate from './components/PropsValidate'
 import Event from './components/Event'
 import { GrandFather } from './components/DirectCommunication'
+import { SyncedText } from './components/Input'
 
 export default createDrawerNavigator({
+  SyncedText: {
+    screen: SyncedText
+  },
   GrandFather: {
     screen: () => <GrandFather name='Ronaldo' lastName='Nazario' />
   },
---
+++ lab/src/components/Input.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { View, Text, TextInput } from 'react-native'
+import Default from '../styles/Default'
+
+export const Input = props => 
+  <View>
+    <TextInput value={props.text} style={Default.input} onChangeText={props.callChange}></TextInput>
+  </View>
+
+export default class SyncedText extends Component {
+  
+  state = {
+    text: ''
+  }
+
+  changeText = text => {
+    this.setState({ text })
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={Default.font40}>{this.state.text}</Text>
+        <Input text={this.state.text} callChange={this.changeText} />
+      </View>
+    )
+  }
+}
