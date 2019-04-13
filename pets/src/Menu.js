17ed7b315d0356793a6020ddcf4914bb9c928cb9
Daniel Cavalcante <c0500272@vale.com>
2019-04-13 16:40:49 -0300

    Add spread example

--- pets/src/Menu.js
+++ pets/src/Menu.js
@@ -4,8 +4,12 @@ import { createDrawerNavigator } from 'react-navigation'
 import Hello from './components/Hello'
 import Plataforma from './components/Plataforma'
 import Count from './components/Count'
+import GrandFather from './components/ComunicacaoDireta'
 
 export default createDrawerNavigator({
+    GrandFather: {
+      screen: () => <GrandFather name='Ronaldo' lastName='Nazario' />
+    },
     Count: {
       screen: () => <Count />
     },
