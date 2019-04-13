9ecd91252fb62d437abb35aca469d2fb8e55c5f4
Daniel Cavalcante <c0500272@vale.com>
2019-04-13 15:08:06 -0300

    Add new components

---
+++ pets/src/components/Count.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableHighlight } from 'react-native'
+
+export default class Count extends Component {
+
+  state = {
+    number: 0
+  }
+
+  increment = () => {
+    this.setState({number: this.state.number + 1})
+  }
+
+  clear = () => {
+    this.setState({number: 0})
+  }
+
+  render() {
+    this.props.number++
+    return (
+      <View>
+        <Text style={{fontSize: 40}}>{this.state.number}</Text>
+        <TouchableHighlight onPress={this.increment} onLongPress={this.clear}>
+          <Text>Incrementar/Zerar</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
