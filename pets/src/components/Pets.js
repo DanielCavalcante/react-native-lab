5881da4c2308b3eb24578214b58f056ac5a7ef82
Daniel Cavalcante <c0500272@vale.com>
2019-05-03 17:02:06 -0300

    Finish style

--- pets/src/components/Pets.js
+++ pets/src/components/Pets.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 import api from '../services/api'
 
 export default class Pets extends Component {
-
   state = {
     pets: []
   }
@@ -19,21 +18,54 @@ export default class Pets extends Component {
   }
 
   renderItem = ({ item }) => (
-    <View>
-      <Text>{item.name}</Text>
-      <Text>{item.raca}</Text>
+    <View style={styles.petContainer}>
+      <Text style={styles.title}>{item.name}</Text>
+      <Text style={styles.raca}>{item.raca}</Text>
     </View>
   )
   
   render() {
     return (
-      <View>
-        <Text>Pets</Text>
+      <View style={styles.container}>
+        <Text style={styles.header}>Pets</Text>
         {/* {this.state.pets.map(pet => {return <Text key={pet._id}>{pet.name}</Text>})} */}
-        <FlatList data={this.state.pets} keyExtractor={item => item._id} renderItem={this.renderItem} />
+        <FlatList data={this.state.pets} keyExtractor={item => item._id} renderItem={this.renderItem}
+          style={styles.list} />
       </View>
     )
   }
-
 }
 
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 20,
+    marginTop: 30,
+    textAlign: 'center'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#FAFAFA'
+  },
+  list: {
+    padding: 20
+  },
+  petContainer: {
+    backgroundColor: '#FFF',
+    borderWidth: 1,
+    borderColor: '#DDD',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  raca: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 3,
+    lineHeight: 24
+  }
+})
