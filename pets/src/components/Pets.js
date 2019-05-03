691f5fe9ac38c3c631022f0b2bd7a11ecdc1b7ad
Daniel Cavalcante <c0500272@vale.com>
2019-05-03 16:50:12 -0300

    Add FlatList for render pets

--- pets/src/components/Pets.js
+++ pets/src/components/Pets.js
@@ -17,12 +17,20 @@ export default class Pets extends Component {
     const pets = response.data;
     this.setState({ pets })
   }
+
+  renderItem = ({ item }) => (
+    <View>
+      <Text>{item.name}</Text>
+      <Text>{item.raca}</Text>
+    </View>
+  )
   
   render() {
     return (
       <View>
         <Text>Pets</Text>
-        {this.state.pets.map(pet => {return <Text key={pet._id}>{pet.name}</Text>})}
+        {/* {this.state.pets.map(pet => {return <Text key={pet._id}>{pet.name}</Text>})} */}
+        <FlatList data={this.state.pets} keyExtractor={item => item._id} renderItem={this.renderItem} />
       </View>
     )
   }
