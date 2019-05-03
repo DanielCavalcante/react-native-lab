44661d98aec79a24e7958f2af111782b12d0712c
Daniel Cavalcante <c0500272@vale.com>
2019-05-03 13:32:25 -0300

    Add DatePicker for Android

--- tasks/src/screens/AddTask.js
+++ tasks/src/screens/AddTask.js
@@ -8,7 +8,9 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   TouchableOpacity,
-  Alert
+  Alert,
+  DatePickerAndroid,
+  Platform
 } from 'react-native'
 import moment from 'moment'
 import commonStyle from '../commonStyle'
@@ -28,7 +30,31 @@ export default class AddTask extends Component {
     this.setState({ ...initialState })
   }
 
+  handleDateAndroidChanged = () => {
+    DatePickerAndroid.open({
+      date: this.state.date
+    }).then(e => {
+      if (e.action != DatePickerAndroid.dismissedAction) {
+        const momentDate = moment(this.state.date)
+        momentDate.date(e.day)
+        momentDate.month(e.month)
+        momentDate.year(e.year)
+        this.setState({ date: momentDate.toDate() })
+      }
+    })
+  }
+
   render() {
+    let datePicker = null
+    if (Platform.OS === 'ios') {
+      datePicker = <DatePickerIOS mode='date' date={this.state.date} onDateChange={date => this.setState({ date })} />
+    } else {
+      datePicker = <TouchableOpacity onPress={this.handleDateAndroidChanged}>
+        <text style={styles.date}>
+          {moment(this.state.date).format('ddd, D [de] MMMM [de] YYYY')}
+        </text>
+      </TouchableOpacity>
+    }
     return (
       <Modal 
         onRequestClose={this.props.onCancel} 
@@ -41,8 +67,7 @@ export default class AddTask extends Component {
           <Text style={styles.header}>New Task</Text>
           <TextInput placeholder="Description" style={styles.input}
             onChangeText={desc => this.setState({ desc })} value={this.state.desc} />
-          <DatePickerIOS mode='date' date={this.state.date} 
-            onDateChange={date => this.setState({ date })} />
+          {datePicker}
           <View style={{flexDirection: 'row', justifyContent: 'flex-end'}}>
             <TouchableOpacity onPress={this.props.onCancel}>
               <Text style={styles.button}>Cancel</Text>
@@ -90,5 +115,11 @@ let styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#e3e3e3',
     borderRadius: 6
+  },
+  date: {
+    fontSize: 20,
+    marginLeft: 10,
+    marginTop: 10,
+    textAlign: 'center'
   }
 })
